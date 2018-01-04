3521a847f03047326af843f50ec1f7d0de18818c
Adam <adam.sorbie@tum.de>
2018-01-04 22:56:20 +0000

    added relative abundance

--- Normalise_OTU_table.py
+++ Normalise_OTU_table.py
@@ -1,32 +1,29 @@
 
-# coding: utf-8
 
-# In[6]:
 
 import pandas as pd
 import numpy
 
-input_1 = raw_input("Enter OTU table filename: ")
+input_file = input("Enter OTU table filename: ")
+input_selection = input("0 = Normalization, 1 = Relative Abundance: ")
 
-
-otu_table = input_1
+otu_table = input_file
 data = pd.read_csv(otu_table , sep=',', index_col=0, skiprows=[1])
 
 
-# In[8]:
+if input_selection == "0":
+    sum_row = data.sum()
+    min_sum = min(data.sum())
+    normalise = data / sum_row * min_sum
+    normalise.to_csv('normalised_OTU.csv', sep=',', encoding='utf-8')
 
-sum_row = data.sum()
-min_sum = min(data.sum())
-normalise = data / sum_row * min_sum
-print(normalise)
+elif input_selection == "1":
+    sum_row = otu.sum()
+    min_sum = min(otu.sum())
+    rel_abund = otu * 100 / sum_row
+    rel_abund.to_csv('relative_abundance.csv', sep=',', encoding='utf-8')
 
 
-# In[9]:
-
-normalise.to_csv('normalised_OTU.csv', sep=',', encoding='utf-8')
-
-
-# In[ ]:
 
 
 
